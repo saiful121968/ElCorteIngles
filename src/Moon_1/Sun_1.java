ee3b7b3d1203f69b5bd9e793bd049fd4ee977301
saiful <saiful121968@gmail.com>
2018-01-05 14:24:46 -0500

    First file added

---
+++ src/Moon_1/Sun_1.java
@@ -0,0 +1,16 @@
+package Moon_1;
+
+public class Sun_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String S1, S2;
+		S1 = new String ("I love ");
+		S2 = new String ("Java Programming");
+		System.out.print(S1);
+		System.out.println(S2);
+
+	}
+
+}
